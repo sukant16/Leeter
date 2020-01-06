d27568849ba65f61cb4437faef02eb66c3be610d
Sukant Arora <sukant.iitr@gmail.com>
2020-01-06 23:57:16 +0530

    added split a string in balanced strings

---
+++ easy/split-a-string-in-balanced-strings.py
@@ -0,0 +1,45 @@
+'''
+Balanced strings are those who have equal quantity of 'L' and 'R' characters.
+
+Given a balanced string s split it in the maximum amount of balanced strings.
+
+Return the maximum amount of splitted balanced strings.
+
+
+
+Example 1:
+
+Input: s = "RLRRLLRLRL"
+Output: 4
+Explanation: s can be split into "RL", "RRLL", "RL", "RL", each substring contains same number of 'L' and 'R'.
+Example 2:
+
+Input: s = "RLLLLRRRLR"
+Output: 3
+Explanation: s can be split into "RL", "LLLRRR", "LR", each substring contains same number of 'L' and 'R'.
+'''
+
+
+class Solution:
+    def balancedStringSplit(self, s: str) -> int:
+        i = count = 0
+        j = 2
+        while i < len(s)-1:
+            if s[i:i+j].count('R') == s[i:i+j].count('L'):
+                count += 1
+                i += j
+                j = 2
+            else:
+                j += 2
+
+        return count
+
+
+if __name__=="__main__":
+
+    sol = Solution()
+    ans1 = sol.balancedStringSplit("RLRRLLRLRL")
+    assert (ans1 == 4), 'wrong'
+
+    ans2 = sol.balancedStringSplit("RLLLLRRRLR")
+    assert (ans2 == 3), 'wrong'
