74ba16a2f1db82843c266379c4e73790a9c44fef
Sukant Arora <sukant.iitr@gmail.com>
2020-10-01 11:55:23 +0530

    first missing positive

---
+++ medium/first-missing-positive.py
@@ -0,0 +1,13 @@
+'''
+Given an unsorted integer array, find the smallest missing positive integer.
+
+Example 1:
+
+Input: [1,2,0]
+Output: 3
+
+find min and max
+if min > 1 or max < 0 -> return 1
+elif min <= 1 -> return 1
+if min < 1 -> return
+'''
