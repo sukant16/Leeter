8c0dd3a16a05a9ef0902f5910765669e244cf16a
Sukant Arora <sukant.iitr@gmail.com>
2021-01-04 01:11:25 +0530

    XOR solution

--- easy/single-number.py
+++ easy/single-number.py
@@ -5,7 +5,7 @@ from collections import  defaultdict
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         """
-        Time complexity should be O(n) and space complexity should be O(1)
+        Time complexity O(n) and space complexity O(n)
         """
         # n = len(nums)
         # if n == 1:
@@ -17,9 +17,24 @@ class Solution:
                 _ = counts.pop(i)
         return list(counts.keys())[0]
 
+    def xor_solution(self, nums: List[int]) -> int:
+        """
+        Time complexity O(n) and space complexity should be O(1)
+        """
+
+        a = 0
+        for num in nums:
+            a ^= num
+
+        return a
+
 
 if __name__ == '__main__':
     sol = Solution()
     print(sol.singleNumber([4,1,2,1,2]))
     print(sol.singleNumber([4]))
     print(sol.singleNumber([2,2,1]))
+
+    print(sol.xor_solution([4,1,2,1,2]))
+    print(sol.xor_solution([4]))
+    print(sol.xor_solution([2,2,1]))
